40c01bc1af83d28d1dbbcf5b313d2f83c05e62b4
joemar-ceneza <joemar.ceneza@gmail.com>
2024-11-07 19:33:31 +0800

    Refractor product components to match updated product data structure

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,39 +1,31 @@
-// link
 import { Link } from "react-router-dom";
 
 export default function Product({ product }) {
   return (
-    <Link to={`/product/${product.id}`}>
+    <Link to={`/product/${product._id}`}>
       <div className="grad w-full h-[362px] rounded-[8px] overflow-hidden relative group">
-        {product.attributes.isNew ? (
+        {product.isNewProduct ? (
           <div className="absolute bg-accent text-primary text-[12px] font-extrabold uppercase top-4 right-4 px-2 rounded-full z-10">
             new
           </div>
         ) : (
           ""
         )}
-        {/* image */}
         <div className="w-full h-[200px] flex items-center justify-center relative">
           <img
             className="w-[160px] h-[160px] group-hover:scale-90 transition-all"
-            src={`${product.attributes.image.data.attributes.url}`}
+            src={`${product.image}`}
             alt=""
           />
         </div>
-        {/* text */}
         <div className="px-6 pb-8 flex flex-col">
-          {/* category title */}
           <div className="text-sm text-accent capitalize mb-2">
-            {product.attributes.categories.data[0]?.attributes.title}
+            {product.category}
           </div>
-          {/* title */}
           <div className="text-[15px] mb-4 lg:mb-9">
-            {product.attributes.title.substring(0, 35)}...
+            {product.title.substring(0, 35)}...
           </div>
-          {/* price */}
-          <div className="text-lg text-accent">
-            $ {product.attributes.price}
-          </div>
+          <div className="text-lg text-accent">$ {product.regularPrice}</div>
         </div>
       </div>
     </Link>
