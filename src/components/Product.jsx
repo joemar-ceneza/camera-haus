6495ac42cb8e1e81ab77a896321230c663492dbb
joemar-ceneza <joemar.ceneza@gmail.com>
2024-12-08 12:32:39 +0800

    Changed from "product.category" to "product.categories"

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Product({ product }) {
   return (
-    <Link to={`/product/${product._id}`}>
+    <Link to={`/product/${product.slug}`}>
       <div className="grad w-full h-[362px] rounded-[8px] overflow-hidden relative group">
         {product.isNewProduct ? (
           <div className="absolute bg-accent text-primary text-[12px] font-extrabold uppercase top-4 right-4 px-2 rounded-full z-10">
@@ -20,7 +20,7 @@ export default function Product({ product }) {
         </div>
         <div className="px-6 pb-8 flex flex-col">
           <div className="text-sm text-accent capitalize mb-2">
-            {product.category}
+            {product.categories}
           </div>
           <div className="text-[15px] mb-4 lg:mb-9">
             {product.title.substring(0, 35)}...
