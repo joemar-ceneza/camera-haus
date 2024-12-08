b6e1133cc9052e5907a8e9627eded9ec2026c530
joemar-ceneza <joemar.ceneza@gmail.com>
2024-12-08 12:23:09 +0800

    Removing unnecessary "attributes"

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -9,12 +9,13 @@ import { useContext } from "react";
 export default function ProductDetails() {
   const { addToCart } = useContext(CartContext);
   const { id } = useParams();
-  const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
+  const { data } = useFetch(`/products/products?slug=${id}`);
+
   if (!data) {
     return <div className="container mx-auto">loading...</div>;
   }
   // category title
-  const categoryTitle = data[0].attributes.categories.data[0].attributes.title;
+  const categoryTitle = data[0].title;
 
   return (
     <div className="mb-16 pt-44 lg:pt-[30px] xl:pt-0">
@@ -23,19 +24,19 @@ export default function ProductDetails() {
           <div className="flex-1 lg:max-w-[40%] lg:h-[540px] grad rounded-lg flex justify-center items-center">
             <img
               className="w-full max-w-[65%]"
-              src={`${data[0].attributes.image.data.attributes.url}`}
+              src={`${data[0].image}`}
               alt=""
             />
           </div>
           <div className="flex-1 bg-primary p-12 xl:p-20 rounded-lg flex flex-col justify-center">
             <div className="uppercase text-accent text-lg font-medium mb-2">
-              {data[0].attributes.categories.data[0].attributes.title} cameras
+              {data[0].title} cameras
             </div>
-            <h2 className="h2 mb-4">{data[0].attributes.title}</h2>
-            <p className="mb-12">{data[0].attributes.description}</p>
+            <h2 className="h2 mb-4">{data[0].title}</h2>
+            <p className="mb-12">{data[0].description}</p>
             <div className="flex items-center gap-x-8">
               <div className="text-3xl text-accent font-semibold">
-                ${data[0].attributes.price}
+                ${data[0].regularPrice}
               </div>
               <button
                 onClick={() => addToCart(data, id)}
